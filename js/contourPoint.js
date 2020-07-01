9064245c509d8c5a79bbd1036ee10188cff18240
bottomleya <bottomleya@hotmail.com>
2020-07-01 16:24:05 +1000

    Update contourPoint.js

--- js/contourPoint.js
+++ js/contourPoint.js
@@ -16,9 +16,34 @@ class contourPoint {
         this.d = this.minD;
         this.ctx = ctx;
         
+        this.colourMap = [
+            { p: 0.0, color: { r: 0xff, g: 0x00, b: 0, a: 0.5} },
+            { p: 0.5, color: { r: 0xff, g: 0xff, b: 0, a: 0.5} },
+            { p: 1.0, color: { r: 0x00, g: 0xff, b: 0, a: 0.5} } ];
+        
     }
-    // converts a percentage value (0-100) to a HEX value
-    // adapted from https://gist.github.com/mlocati/7210513
+    // converts a percentage value (0-100) to a RGBA value
+    // adapted from https://stackoverflow.com/questions/7128675/from-green-to-red-color-depend-on-percentage
+    percentageToColour(p) {
+        for (var i = 1; i < this.colourMap.length - 1; i++) {
+            if (p < this.colourMap[i].p) {
+                break;
+            }
+        }
+        var lower = this.colourMap[i - 1];
+        var upper = this.colourMap[i];
+        var range = upper.p - lower.p;
+        var rangePct = (p - lower.p) / range;
+        var pctLower = 1 - rangePct;
+        var pctUpper = rangePct;
+        var color = {
+            r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+            g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+            b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper),
+            a: a
+        };
+        return 'rgba(' + [color.r, color.g, color.b, color.a].join(',') + ')';
+    };
     percentageToColour(p) {
         var r, g, b = 0;
         if(p < 50) {
