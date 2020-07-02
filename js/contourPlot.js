e94989a28e4e3034dfb608cf7656ecfd8a501566
bottomleya <bottomleya@hotmail.com>
2020-07-02 11:17:00 +1000

    Update contourPlot.js

--- js/contourPlot.js
+++ js/contourPlot.js
@@ -11,6 +11,8 @@ class contourPlot {
 
       this.sizeWidth = ctx.canvas.clientWidth;
       this.sizeHeight = ctx.canvas.clientHeight;
+      this.widthTotPx = Math.Floor(this.sizeWidth / Math.sqrt(this.granularity))
+      this.widthTotPx = Math.Floor(this.sizeHeight / Math.sqrt(this.granularity))
 
       this.colourMap = [
           { p: 0.0, color: { r: 0x00, g: 0xff, b: 0, a: 0.8} },
@@ -18,13 +20,24 @@ class contourPlot {
           { p: 1.0, color: { r: 0xff, g: 0x00, b: 0, a: 0.4} } ];
     }
     drawPlot() {
-    this.clearCanvas();
-      for (var i=0; i<this.points.length; i++) {
-        console.log(this.points[i].x);
-      }
+        // clear canvas
+        this.clearCanvas();
+        // loop width
+        for (var i=0; i<this.widthTotPx; i++) {
+            // loop height
+            for (var j=0; j<this.heightTotPx; j++) {
+                // calculate average val
+                for (var k=0; k<this.points.length; k++) {
+                    console.log(this.points[k].x);
+                }
+            }
+        }
     }
     clearCanvas() {
-      this.ctx.clearRect(0,0,c.sizeWidth,c.sizeHeight);
+      this.ctx.clearRect(0,0,this.sizeWidth,this.sizeHeight);
+    }
+    drawSquare(x, y, size, style) {
+        
     }
 }
     
