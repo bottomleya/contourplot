76cc8bed75579e1e8de89d0e8c0c8107922be8ea
bottomleya <bottomleya@hotmail.com>
2020-07-03 12:24:31 +1000

    Update contourPoint.js

--- js/contourPoint.js
+++ js/contourPoint.js
@@ -16,33 +16,8 @@ class contourPoint {
         this.d = this.minD;
         this.ctx = ctx;
         
-        this.colourMap = [
-            { p: 0.0, color: { r: 0x00, g: 0xff, b: 0, a: 0.8} },
-            { p: 0.5, color: { r: 0xff, g: 0xff, b: 0, a: 0.5} },
-            { p: 1.0, color: { r: 0xff, g: 0x00, b: 0, a: 0.4} } ];
+        this.colourMap = new colourMap("warm-50");
         
-    }
-    // converts a percentage value (0-1) to a RGBA value
-    // adapted from https://stackoverflow.com/questions/7128675/from-green-to-red-color-depend-on-percentage
-    percentageToColour(p) {
-        for (var i = 1; i < this.colourMap.length - 1; i++) {
-            if (p < this.colourMap[i].p) {
-                break;
-            }
-        }
-        var lower = this.colourMap[i - 1];
-        var upper = this.colourMap[i];
-        var range = upper.p - lower.p;
-        var rangePct = (p - lower.p) / range;
-        var pctLower = 1 - rangePct;
-        var pctUpper = rangePct;
-        var color = {
-            r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
-            g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
-            b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper),
-            a: lower.color.a * pctLower + upper.color.a * pctUpper
-        };
-        return 'rgba(' + [color.r, color.g, color.b, color.a].join(',') + ')';
     }
     setValue(val) {
         // Limit value to maximum amount
@@ -65,7 +40,7 @@ class contourPoint {
         this.setValue(this.val + this.baseGrowthRate*Math.sqrt(this.d));
     }
     colourIn() {
-        this.ctx.fillStyle = this.percentageToColour(this.val);
+        this.ctx.fillStyle = this.colourMap.percentageToColour(this.val);
         this.ctx.beginPath();
         this.ctx.arc(this.x, this.y, this.d/2, 0, 2 * Math.PI);
         this.ctx.fill();
