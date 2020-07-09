3860a0aeba9f51ac0b8cc86795d752bb52f45a25
bottomleya <bottomleya@hotmail.com>
2020-07-09 16:06:26 +1000

    Update colourMap.js

--- js/colourMap.js
+++ js/colourMap.js
@@ -21,25 +21,27 @@ class colourMap {
   }
   // converts a percentage value (0-1) to a RGBA value
   // adapted from https://stackoverflow.com/questions/7128675/from-green-to-red-color-depend-on-percentage
-  percentageToColour(p) {
-        for (var i = 1; i < this.colourMap.length - 1; i++) {
-            if (p < this.colourMap[i].p) {
-                break;
-            }
+  percentageToColour(p, returnString) {
+    returnString = returnString || True
+    for (var i = 1; i < this.colourMap.length - 1; i++) {
+        if (p < this.colourMap[i].p) {
+            break;
         }
-        var lower = this.colourMap[i - 1];
-        var upper = this.colourMap[i];
-        var range = upper.p - lower.p;
-        var rangePct = (p - lower.p) / range;
-        var pctLower = 1 - rangePct;
-        var pctUpper = rangePct;
-        var color = {
-            r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
-            g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
-            b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper),
-            a: lower.color.a * pctLower + upper.color.a * pctUpper
-        };
-        return 'rgba(' + [color.r, color.g, color.b, color.a].join(',') + ')';  
+    }
+    var lower = this.colourMap[i - 1];
+    var upper = this.colourMap[i];
+    var range = upper.p - lower.p;
+    var rangePct = (p - lower.p) / range;
+    var pctLower = 1 - rangePct;
+    var pctUpper = rangePct;
+    var color = {
+        r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+        g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+        b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper),
+        a: lower.color.a * pctLower + upper.color.a * pctUpper
+    };
+    if (returnString) {return 'rgba(' + [color.r, color.g, color.b, color.a].join(',') + ')'}; 
+    return color;
   }
   //
     hex2Rgb(hex) {
