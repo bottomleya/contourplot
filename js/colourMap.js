03e41d73da44499b087d18b69a1e4f0d081f4553
bottomleya <bottomleya@hotmail.com>
2020-07-09 15:57:48 +1000

    Update colourMap.js

--- js/colourMap.js
+++ js/colourMap.js
@@ -41,4 +41,21 @@ class colourMap {
         };
         return 'rgba(' + [color.r, color.g, color.b, color.a].join(',') + ')';  
   }
+  //
+    hex2Rgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+    componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
+
+    rgb2Hex(r, g, b) {
+        return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
+    }
 }
